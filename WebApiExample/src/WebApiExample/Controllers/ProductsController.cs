3478cbdcbb22201307a6af4564ff064c78525e7b
Jonathan Cruz Rocha Ferreira <jonathan.ferreira@animaeducacao.com.br>
2015-09-23 17:02:15 -0300

    First

---
+++ WebApiExample/src/WebApiExample/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNet.Mvc;
+using System.Net;
+using WebApiExample.Infraestrutura.Dados.Entidades;
+using WebApiExample.Infraestrutura.Dados.Repositorios;
+
+namespace WebApiExample.Controllers
+{
+    [Route("api/produtos")]
+    public class ProductsController : Controller
+    {
+        private static ProdutoRepositorio Repositorio = new ProdutoRepositorio();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var produtos = Repositorio.ObterProdutos();
+
+            Context.Response.StatusCode = (int)HttpStatusCode.Found;
+            return new ObjectResult(produtos);
+        }
+
+        [HttpGet("{id}", Name = "ObterProduto")]
+        public IActionResult Get(int id)
+        {
+            var produto = Repositorio.Obter(id);
+            if (produto == null)
+                return HttpNotFound();
+
+            Context.Response.StatusCode = (int)HttpStatusCode.Found;
+            return new ObjectResult(produto);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Produto produto)
+        {
+            var id = Repositorio.Criar(produto.Name, produto.Value);
+
+            string url = Url.RouteUrl("ObterProduto", new { id }, Request.Scheme, Request.Host.ToUriComponent());
+
+            Context.Response.Headers["Location"] = url;
+            return new HttpStatusCodeResult((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Produto ProdutoRequest)
+        {
+            var produto = Repositorio.Obter(id);
+            if (produto == null)
+                return HttpNotFound();
+
+            if (id != ProdutoRequest.Id)
+                return HttpBadRequest("Post the attribute: Id properly");
+
+            ProdutoRequest.Id = id;
+            Repositorio.Update(ProdutoRequest);
+
+            return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = Repositorio.Obter(id);
+            if (produto == null)
+                return HttpNotFound();
+
+            Repositorio.Remover(id);
+
+            return new HttpStatusCodeResult((int)HttpStatusCode.OK);
+        }
+    }
+}
---
+++ WebApiExample/src/WebApiExample/Infraestrutura/Dados/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApiExample.Infraestrutura.Dados.Entidades;
+
+namespace WebApiExample.Infraestrutura.Dados.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        private readonly List<Produto> Produtos = new List<Produto>();
+
+        public ProdutoRepositorio()
+        {
+            Enumerable.Range(1, 100)
+                .ToList()
+                .ForEach(x =>
+                {
+                    Produtos.Add(new Produto { Id = x, Name = $"Produto {x}", Value = x });
+                });
+        }
+
+        public IEnumerable<Produto> ObterProdutos()
+        {
+            return Produtos;
+        }
+
+        public Produto Obter(int id)
+        {
+            return Produtos.SingleOrDefault(x => x.Id == id);
+        }
+
+        public int Criar(string nome, decimal valor)
+        {
+            var proximoId = Produtos.Select(x => x.Id).Max() + 1;
+
+            Produtos.Add(new Produto { Id = proximoId, Name = nome, Value = valor });
+
+            return proximoId;
+        }
+
+        public void Remover(int id)
+        {
+            Produtos.RemoveAll(x => x.Id == id);
+        }
+
+        public void Update(Produto entidade)
+        {
+            var produto = Obter(entidade.Id);
+            if (produto == null)
+                return;
+
+            produto.Name = entidade.Name;
+            produto.Value = entidade.Value;
+        }
+    }
+}
